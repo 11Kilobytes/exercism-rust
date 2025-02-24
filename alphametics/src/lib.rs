fc15d202c66db07eaa2a6f195583dcf4b63c5bf5
Kabelo M'sobomvu Moiloa <elevenkb@tilde.club>
2025-02-24 18:29:13 -0500

    alphametics: Support an arbitrary number of arguments in a Puzzle.

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -5,96 +5,105 @@ use std::collections::HashMap;
 
 #[derive(Copy, Clone, PartialEq, Eq)]
 enum Cell {
+    /// Varaible(v) :: a variable with the name v.  Where 0 <= v < 26.
     Variable(u8),
+    /// Digit(d) :: a fixed digit d.
     Digit(u8),
 }
 
-/// A valid Puzzle representing A + B == C, where a is the
-/// _little-endian_ encoding of A and likewise with b, c.
+impl Cell {
+    fn is_var(&self) -> bool {
+        match self {
+            Cell::Variable(_) => true,
+            _ => false,
+        }
+    }
+    fn is_digit(&self) -> bool {
+        match self {
+            Cell::Digit(_) => true,
+            _ => false,
+        }
+    }
+}
+/// A valid Puzzle representing the equation sum(args) == target,
+/// where each entry of args is a decimal number represented as a
+/// _little-endian_ vector of digits, and likewise with target.
 #[derive(Clone)]
 struct Puzzle {
-    a: Vec<Cell>,
-    b: Vec<Cell>,
-    c: Vec<Cell>,
+    args: Vec<Vec<Cell>>,
+    target: Vec<Cell>,
     carry: Vec<bool>,
     var_to_digit: [Option<u8>; 26],
-    /// We assume that 0 <= col < len where len is the common length
-    /// of the vector members of a valid Puzzle.
+    /// We assume that 0 <= col <= len where len is the common length
+    /// of the elements of args.
     col: usize,
 }
-enum Row {
-    A,
-    B,
-}
+
 struct Move {
-    row: Row,
     var: u8,
     digit: u8,
 }
 
 impl Puzzle {
-    fn subst(&self, m: Move) -> Option<Self> {
+    fn subst_row(row: &mut Vec<Cell>, var_to_digit: &[Option<u8>; 26]) {
+        for c in row.iter_mut() {
+            if let Cell::Variable(v) = *c {
+                if let Some(d) = var_to_digit[v as usize] {
+                    *c = Cell::Digit(d);
+                }
+            }
+        }
+    }
+    fn subst(&mut self, m: Move) {
+        self.var_to_digit[m.var as usize] = Some(m.digit);
+        for row in self.args.iter_mut() {
+            Puzzle::subst_row(row, &self.var_to_digit);
+        }
+        Puzzle::subst_row(&mut self.target, &self.var_to_digit);
+    }
+    pub fn play(&self, m: Move) -> Option<Self> {
         debug_assert!((0..26).contains(&m.var));
         let mut result = self.clone();
-        result.var_to_digit[m.var as usize] = Some(m.digit);
-        for i in result.col..result.c.len() {
-            if let Cell::Variable(v) = result.a[i] {
-                if let Some(d) = result.var_to_digit[v as usize] {
-                    result.a[i] = Cell::Digit(d);
-                }
-            }
-            if let Cell::Variable(v) = result.b[i] {
-                if let Some(d) = result.var_to_digit[v as usize] {
-                    result.b[i] = Cell::Digit(d);
-                }
-            }
-            if let Cell::Variable(v) = result.c[i] {
-                if let Some(d) = result.var_to_digit[v as usize] {
-                    result.c[i] = Cell::Digit(d);
-                }
-            }
-            if let (Cell::Digit(a_i), Cell::Digit(b_i)) = (result.a[i], result.b[i]) {
-                let sum = a_i + b_i + (result.carry[i] as u8);
-                let (carry, d) = (sum / 10, sum % 10);
-                match result.c[i] {
-                    Cell::Variable(v) => {
-                        result.var_to_digit[v as usize] = Some(d);
-                        result.c[i] = Cell::Digit(d);
-                    }
-                    Cell::Digit(c_i) => {
-                        if c_i != d {
+        result.subst(m);
+        for col in result.col..result.target.len() {
+            if result.args.iter().all(|arg| arg[col].is_digit()) {
+                let mut sum: u8 = result
+                    .args
+                    .iter()
+                    .map(|arg| match arg[col] {
+                        Cell::Variable(_) => 0,
+                        Cell::Digit(d) => d,
+                    })
+                    .sum();
+                sum += result.carry[col] as u8;
+                let (carry_next, digit) = (sum / 10, sum % 10);
+                match result.target[col] {
+                    Cell::Variable(var) => result.subst(Move { var, digit }),
+                    Cell::Digit(d) => {
+                        if digit != d {
                             return None;
                         }
                     }
                 }
-                if carry != 0 {
-                    *result.carry.get_mut(i + 1)? = true;
+                if carry_next != 0 {
+                    *result.carry.get_mut(col + 1)? = true;
                 }
             }
         }
-        result.col += result.a[result.col..]
-            .iter()
-            .zip(result.b[result.col..].iter())
-            .position(|p| match p {
-                (Cell::Digit(_), Cell::Digit(_)) => true,
-                _ => false,
-            })
-            .map(|i| i + 1)
-            .unwrap_or(0);
+        result.col += (result.col..result.target.len())
+            .take_while(|&col| result.args.iter().all(|arg| arg[col].is_digit()))
+            .count();
         return Some(result);
     }
-    fn succ(&self) -> impl Iterator<Item = Self> {
+    pub fn succ(&self) -> impl Iterator<Item = Self> {
         todo!()
     }
 }
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    let words: [Vec<u8>; 3] = input
+    let words: Vec<Vec<u8>> = input
         .split(|c: char| !c.is_ascii_uppercase())
         .map(|word| word.bytes().map(|b| b - b'A').collect::<Vec<u8>>())
-        .collect::<Vec<Vec<u8>>>()
-        .try_into()
-        .expect("Invalid Puzzle");
-    let (wa, wb, wc) = words.into();
+        .collect();
     todo!()
 }
