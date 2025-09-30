fb486a8767270713c8aeec84922808636591f5cd
Kabelo M'sobomvu Moiloa <code@kabelom.com>
2025-09-30 19:33:31 +0200

    channels-lock: Update

--- channels-lock.scm
+++ channels-lock.scm
@@ -3,7 +3,7 @@
         (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "2e476f9ad87e99a2a819118d2eaed5bcf3c8d309")
+          "283f64c9bbf4f775c121f0ee38f914563db9ebdd")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
